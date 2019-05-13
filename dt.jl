dd4efba164437108502a866f7ce57943af7a47ab
Alexandre Werneck <xanwerneck@gmail.com>
2019-05-12 23:36:03 -0300

    adjust the accuracy compute

--- dt.jl
+++ dt.jl
@@ -1,170 +1,5 @@
-using DataFrames
-using CSV
 using Statistics
-using Random
-
-mutable struct Node
-    data::DataFrame
-    index::Int64
-    gini::Float64
-    isLeaf::Bool
-    way::String
-    mean::Any
-    feature::Int64
-    nodeTrue::Node
-    nodeFalse::Node
-    # Change the construct of struct
-    Node(data,index,gini,isLeaf,way,mean,feature) = new(data,index,gini,isLeaf,way,mean,feature)
-end
-
-function ComputeEntropy(S, M)
-    Y_uniques = unique(S[:,M])
-    S_entropy = 0.0
-    N         = size(S,1)
-
-    for y in range(1, length=size(Y_uniques, 1))
-        p_y = count(x->(x==Y_uniques[y]),S[:,M]) / N
-        S_entropy += -(p_y * log(p_y))
-    end
-    
-    return S_entropy
-end
-
-function InformationGain(S, S_left, S_right, M)
-    Avg_left  = size(S_left,1) / size(S,1)
-    Avg_right = size(S_right,1) / size(S,1)
-    
-    return ComputeEntropy(S,M) - (Avg_left * ComputeEntropy(S_left,M)) - (Avg_right * ComputeEntropy(S_right,M))
-end
-
-function ComputeInformationGain(S, Feature)
-    #Number of columns
-    N, M = size(S)
-    #Filter just one feature
-    data_impurity = S[:,Feature]
-    
-    #Distinct values of dataframe
-    unique_values_feature = unique(data_impurity)
-    
-    #Minimun of impurity
-    gain_information = (0.,0.,0)
-    
-    for value_feature in unique_values_feature
-
-        S_left  = filter(x -> (x[:][Feature] <= value_feature), S)
-        S_right = filter(x -> (x[:][Feature] > value_feature), S)
-        
-        gain = InformationGain(S, S_left, S_right, M)
-        
-        if (gain > gain_information[2])
-            gain_information = (value_feature, gain, Feature)
-        end
-    end
-
-    return gain_information
-end
-
-function GImpurity(S, Feature)
-    #Number of columns
-    N, M = size(S)
-    #Filter just one feature
-    data_impurity = S[:,Feature]
-    #Distinct values of dataframe
-    unique_feature = sort(unique(data_impurity))
-    
-    #Means between intervals
-    unique_means   = []    
-    for i in range(1,length=size(unique_feature,1)-1)
-        push!(unique_means, ( unique_feature[i] + unique_feature[i+1] ) / 2 )
-    end
-    if size(unique_means, 1) == 0
-        unique_means = unique_feature
-    end
-    
-    #Minimun of impurity
-    gini_impurity_feature = (1.,size(unique_means, 1),0)
-    for mean in unique_means
-
-        node_left  = filter(x -> (x[:][Feature] <= mean), S)
-        node_right = filter(x -> (x[:][Feature] > mean), S)
-            
-        gini_impurity_left = 1.0
-        if size(node_left,1) > 0  
-            Y_uniques_left = unique(node_left[:,M])
-            for y in range(1, length=size(Y_uniques_left, 1))
-                gini_impurity_left -= ( count(x->(x==Y_uniques_left[y]),node_left[:,M]) / size(node_left,1) ) ^ 2
-            end
-        end
-                
-        gini_impurity_right = 1.0
-        if size(node_right,1) > 0  
-            Y_uniques_right = unique(node_right[:,M])
-            for y in range(1, length=size(Y_uniques_right, 1))
-                gini_impurity_right -= ( count(x->(x==Y_uniques_right[y]),node_right[:,M]) / size(node_right,1) ) ^ 2
-            end
-        end
-
-        gini_impurity_node = ( ( size(node_left,1) / N ) * gini_impurity_left ) + ( ( size(node_right,1) / N ) * gini_impurity_right )
-        #gini_impurity_node = gini_impurity_left + gini_impurity_right
-        if (gini_impurity_node < gini_impurity_feature[2]) && (gini_impurity_feature[2] > 0.0)
-            gini_impurity_feature = (mean, gini_impurity_node, Feature)
-        end        
-    end
-    return gini_impurity_feature
-end
-
-function GetMin(ItemArray, Index)
-    min     = Inf
-    ret_min = (1.,1.,1.)
-    for item in ItemArray
-        if item[Index] < min
-            min = item[Index]
-            ret_min = item
-        end
-    end
-    return ret_min
-end
-
-function GetMax(ItemArray, Index)
-    max     = 0.
-    ret_max = (0,0.,0.)
-    for item in ItemArray
-        if item[Index] > max
-            max = item[Index]
-            ret_max = item
-        end
-    end
-    return ret_max
-end
-
-function BuildTree(S, NodeFrom, Nodes, Position = 0, Way = "Root")
-    N, M = size(S)
-    # Get the node
-    features_impurity = Array{Tuple{Integer,Float64,Float64}}(undef,0)
-    for j in range(1,length=M-1)
-        mean_imp, gini_imp = ComputeInformationGain(S, j)        
-        push!(features_impurity, (j, mean_imp, gini_imp))
-    end
-    node_min = GetMax(features_impurity,3)
-    
-    if (size( unique(S[:,M]) , 1 ) > 1) && (size( unique( S[:,node_min[1]] ), 1) > 1)
-        node = Node(S, Position, node_min[3], false, Way, node_min[2], node_min[1])
-        # Go to left - true
-        BuildTree(filter(x -> x[:][node_min[1]] <= node_min[2],S), node, Nodes, Position + 1, "True")
-        # Go to right - false
-        BuildTree(filter(x -> x[:][node_min[1]] > node_min[2],S), node, Nodes, Position + 1, "False")
-    else
-        node = Node(S, Position, node_min[3], true, Way, node_min[2], node_min[1])
-    end
-    if Way == "True"
-        NodeFrom.nodeTrue = node
-    end
-    if Way == "False"
-        NodeFrom.nodeFalse = node
-    end
-
-    push!(Nodes, node)    
-end
+using CSV
 
 function TrainTest(S, test_size = 0.25)
     train = S[trunc(Int, floor( size(S,1) * test_size ) ) + 1 : size(S,1),:]
@@ -172,45 +7,195 @@ function TrainTest(S, test_size = 0.25)
     return train, test
 end
 
-function Accuracy(predictions)
-    perc_right_answer = size(filter(x -> x[:][3] == true, predictions), 1) / size(predictions,1)
-    return perc_right_answer * 100
+function entropy(S)
+    y_values  = S[:,2]
+    y_uniques = unique(y_values)
+
+    entropy = 0.
+    for y_unique in y_uniques
+        p_y_unique = count(x -> x == y_unique, y_values) / size(S,1)
+        entropy += p_y_unique * log(p_y_unique)
+    end
+    return -entropy
 end
 
-function Predict(Test, predictions, nodes)
-    Size_test   = size(Test,1)
+function split_dataframe(S, feature, divider)
+    if (feature_types[feature] == String)
+        s_left  = filter(x -> x[:][feature] == divider, S)
+        s_right = filter(x -> x[:][feature] !=  divider, S) 
+    end
+    if (feature_types[feature] == Int64)
+        s_left  = filter(x -> x[:][feature] == divider, S)
+        s_right = filter(x -> x[:][feature] !=  divider, S) 
+    end
+    if (feature_types[feature] == Float64)
+        s_left  = filter(x -> x[:][feature] <= divider, S)
+        s_right = filter(x -> x[:][feature] >  divider, S) 
+    end
+    return s_left, s_right
+end
+
+function informationgain(S, feature)
     
-    for row in eachrow(Test)
-        node = GetRoot(nodes)
-        while !node.isLeaf
-            if row[node.feature] <= node.mean
-                # True
-                node = node.nodeTrue
-            elseif row[node.feature] > node.mean
-                # False
-                node = node.nodeFalse
-            end
+    unique_values = unique(S[:,1])
+
+    max_gain = (0.,0,1)
+    for unique_value in unique_values
+        if (feature_types[feature] == String)
+            s_left  = filter(x -> x[:][1] == unique_value, S)
+            s_right = filter(x -> x[:][1] != unique_value, S) 
         end
-        if node.isLeaf
-            pred_value  = row[size(Test,2)]
-            result_node = (pred_value, unique(node.data[:,size(Test,2)])[1], unique(node.data[:,size(Test,2)])[1] == pred_value)
-            push!(predictions, result_node)
+        if (feature_types[feature] == Int64)
+            s_left  = filter(x -> x[:][1] == unique_value, S)
+            s_right = filter(x -> x[:][1] != unique_value, S) 
+        end
+        if (feature_types[feature] == Float64)
+            s_left  = filter(x -> x[:][1] <= unique_value, S)
+            s_right = filter(x -> x[:][1] > unique_value, S)    
+        end
+
+        gain = entropy(S) - (
+            ( size(s_left,1) / size(S,1) ) * entropy(s_left)
+           +( size(s_right,1) / size(S,1) ) * entropy(s_right)
+        )
+        if (gain > max_gain[1])
+            max_gain = (gain, feature, unique_value)
         end
     end
+    return max_gain
+
 end
 
-function GetRoot(nodes)
+function createnode(S, divider, feature, way, node_left, node_right, isLeaf)
+    node = [
+        S,
+        divider,
+        feature,
+        way,
+        node_left,
+        node_right,
+        isLeaf
+    ]
+    return node
+end
+
+function buildtree(S, way = "I")
+    N,M = size(S)
+
+    # If node is Leaf - dont need to be divided    
+    if ( size( unique(S[:,M]) , 1) == 1 )
+        node = createnode(S, 0, 0, way, 0, 0, true)
+        push!(nodes, node)
+        return size(nodes,1)
+    end
+
+    gains = []
+    for i in range(1,length=M-1)
+        gain = informationgain(S[:,[i,M]], i)
+        push!( gains, gain ) 
+    end
+    
+    max_gain = gains[1]
+    for gain in gains
+        if (gain[1] > max_gain[1])
+            max_gain = gain
+        end
+    end
+    
+    gain, feature, divider = max_gain
+    
+    s_left, s_right = split_dataframe(S, feature, divider)
+    
+    node_left  = buildtree(s_left, "L")
+    node_right = buildtree(s_right, "R")
+
+    node = createnode(S, divider, feature, way, node_left, node_right, false)
+    push!(nodes, node)
+
+    return size(nodes,1)
+   
+end
+
+function getbegginner()
     for node in nodes
-        if node.way == "Root"
+        if (node[4] == "I")
             return node
         end
     end
 end
 
-function TrainTree(train, nodes)
-    # Create a pseudo root node
-    # node_root   = Node(train, 0, 0.,false, "None", 0., 0)
-    
-    # Start creating the tree
-    BuildTree(train, Any, nodes, 0)
+function check_predict(feature, value_1, value_2)
+    if (feature_types[feature] == String)
+        return value_1 == value_2
+    end
+    if (feature_types[feature] == Int64)
+        return value_1 == value_2
+    end
+    if (feature_types[feature] == Float64)
+        return value_1 <= value_2
+    end
 end
+
+function predicttree(Test, predictions)
+    Row_size_test, Col_size_test   = size(Test)
+
+    for row in eachrow(Test)
+        node = getbegginner()
+        #node = nodes[size(nodes,1)] # Last node inserted
+        
+        while node[7] == false
+            if check_predict(node[3], row[node[3]], node[2])
+                # True
+                node = nodes[node[5]]
+            else
+                # False
+                node = nodes[node[6]]
+            end
+            
+        end
+        if node[7] == true
+            pred_value_test  = row[Col_size_test]
+            pred_value_tree  = unique(node[1][:,Col_size_test])[1]
+            pred_value_check = row[Col_size_test] == unique(node[1][:,Col_size_test])[1]
+            
+            result_node = (pred_value_test, pred_value_tree, pred_value_check)
+            push!(predictions, result_node)
+        end
+        
+    end
+end
+
+function traindt(S)
+    buildtree(S)
+end
+
+nodes = []
+predictions = Array{Tuple{Any,Any,Bool}}(undef,0)
+
+S_dataset   = CSV.read(string("data/",ARGS[1]))
+# Build 2 dataframe - train with 75% and test with 25%
+println("---- Split the data ----")
+train, test = TrainTest(S_dataset, 0.25)
+
+# Construct the feature types
+feature_types = []
+for j in range(1, length=size(train, 2))
+    push!(feature_types, eltype(train[j]))
+end
+
+# Train and build a single tree
+# Input: train : dataset of train
+println("---- Train the data ----")
+traindt(train)
+
+# Predict the test dataset
+println("---- Predict the test data ----")
+predicttree(test, predictions)
+
+mape = []
+for i in range(1, length=size(predictions, 1))
+    error_pred = abs(predictions[i][1] - predictions[i][2])
+    push!(mape, 100 * ( error_pred / predictions[i][1] ) )
+end
+accuracy    = 100 - mean(mape)
+@show accuracy
